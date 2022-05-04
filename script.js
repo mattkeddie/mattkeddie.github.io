0b37b8ab65e84f8e9f6304f255e372ba5b549da4
mattkeddie <74267664+mattkeddie@users.noreply.github.com>
2022-05-04 04:13:18 +0100

    adjustments to flex boxes

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("myTopnav").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-80px";
+    document.getElementById("myTopnav").style.top = "-80px";
   }
   prevScrollpos = currentScrollPos;
 }
 
-function myFunction(x) {
-  x.classList.toggle("change");
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
