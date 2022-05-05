a20c6afaaa06c473189cf2b3179ee0bc8c4a37a7
mattkeddie <74267664+mattkeddie@users.noreply.github.com>
2022-05-05 20:00:51 +0100

    made nav fixed on mobile

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
+  if (window.innerWidth < 800) {
+    return;
+  }
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("myTopnav").style.top = "0";
